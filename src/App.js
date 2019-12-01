958c9d491f780f3263d88eec411e42191c759c9d
Dmitry <favoritday@mail.ru>
2019-12-01 14:43:24 +0300

    gdf

--- src/App.js
+++ src/App.js
@@ -2,23 +2,17 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav>
+        <ul>
+          <li><a href="">Главная</a></li>
+          <li><a href="" >Авторизация</a></li>
+          <li><a href="">Профиль</a></li>
+          <li><a href="inf.html">Информация</a></li>
+        </ul>
+      </nav>
     </div>
   );
 }
